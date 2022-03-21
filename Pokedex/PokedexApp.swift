3b024a91d204ffd3a592acf172489f34fc672b32
Edson Lipa <edsonlipa@gmail.com>
2022-03-21 10:43:13 -0500

    inital commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Edson Lipa on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
